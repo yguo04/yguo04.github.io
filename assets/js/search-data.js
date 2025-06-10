715bc4f49b6ddfafe9ce2b8f6fae89e35b0f596d
StarWalkin <794063781@qq.com>
2025-06-10 14:15:25 +0000

    Deploying to gh-pages from @ yguo04/yguo04.github.io@69f7ab8f4a8eac422b374dad6ac5613d2b8167e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-we-have-released-eva-a-red-teaming-framework-gui-agents-with-indirect-prompt-injection",
+          title: 'We have released EVA, a red teaming framework GUI agents with indirect prompt...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
