385af40d5de8bf7484ab5c13f746601af2945408
StarWalkin <794063781@qq.com>
2025-06-12 08:06:15 +0000

    Deploying to gh-pages from @ yguo04/yguo04.github.io@e0b052c5c4d9707b3da9e82271a5a1aa822149c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,14 +466,14 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-open-source-ml-library",
-          title: 'Open Source ML Library',
-          description: "Contributing to popular machine learning framework",
+            },},{id: "projects-awesome-gui",
+          title: 'Awesome-GUI',
+          description: "A curated list of papers, projects, and resources for multi-modal Graphical User Interface (GUI) agents.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/contribute_project_1/";
-            },},{id: "projects-community-research-initiative",
-          title: 'Community Research Initiative',
-          description: "Contributing to collaborative research project",
+            },},{id: "projects-dive-into-llms",
+          title: 'Dive-into-LLMs',
+          description: "I contributed to the GUI agent tutorial of Dive-into-LLMs repository.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/contribute_project_2/";
             },},{id: "projects-ui-nexus",
